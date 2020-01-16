351fbf657f95eb0111e7a70940493f195c1619db
Don Mosites <don.mosites@fluidity.io>
2020-01-16 18:33:51 -0500

    Adding autoupdater plugins

--- src/lib/init-hook.ts
+++ src/lib/init-hook.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk'
 import { Hook } from '@oclif/config'
 
 const hook: Hook<'init'> = async function(options) {
-  console.log(chalk.gray.bold(`\nAirSwap CLI ${options.config.version} — https://support.airswap.io/`))
+  console.log(chalk.gray.bold(`AirSwap CLI ${options.config.version} — https://support.airswap.io/`))
 }
 
 export default hook
