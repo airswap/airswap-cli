c3dec80c09d4fed86a4c40e6b54ad426131b7bad
Don Mosites <don.mosites@fluidity.io>
2020-01-31 17:33:55 -0500

    Adding available update checker

--- src/lib/init-hook.ts
+++ src/lib/init-hook.ts
@@ -1,8 +1,29 @@
 import chalk from 'chalk'
 import { Hook } from '@oclif/config'
+import available from 'available-versions'
+import compare from 'compare-versions'
+import * as emoji from 'node-emoji'
+import { table } from 'table'
 
 const hook: Hook<'init'> = async function(options) {
   console.log(chalk.gray.bold(`AirSwap CLI ${options.config.version} — https://support.airswap.io/`))
+  var query = {
+    name: 'airswap',
+  }
+  const result = await available(query)
+  if (compare(options.config.version, result['dist-tags'].latest) === -1) {
+    console.log()
+    const data = [
+      [
+        `${emoji.get('package')} ${chalk.bold.green('UPDATE AVAILABLE')} Run ${chalk.bold(
+          'yarn global add airswap@latest',
+        )} to upgrade`,
+      ],
+    ]
+    console.log(table(data, {}))
+  } else {
+    console.log()
+  }
 }
 
 export default hook
